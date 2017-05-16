9e2284bc9d5c03764d4af976399229b4c65e2a17
Novulux <jspham@ucsd.edu>
2017-05-16 15:05:39 -0700

    mongo support?

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,8 @@
  * Config var for app
 **/
 module.exports = {
-  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://localhost:27017/webstormtroopers',
+  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI || 'mongodb://admin:admin@ds143201.mlab.com:43201/heroku_sz7vw9qs',
+ // 'mongodb://localhost:27017/webstormtroopers',
   port: process.env.PORT || 4941,
-  secret: process.env.SECRET || 'mysecret'
+  secret: process.env.SECRET || 'mysecret',
 };
