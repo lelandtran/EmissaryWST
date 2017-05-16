30a293e617b7089fb00e67fa6c8bfba22add59e5
Minh Vu <mbvu@ucsd.edu>
2017-05-15 18:02:15 -0700

    Added in api company for dmmo

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-13T22:15:40.901Z",
+    "time": "2017-05-16T00:59:48.310Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
--- server/routes/appointment/appointment.controller.js
+++ server/routes/appointment/appointment.controller.js
@@ -1,15 +1,5 @@
 'use strict';
 
-/**
- * @api {get} /user/:id Request User information
- * @apiName GetUser
- * @apiGroup User
- *
- * @apiParam {Number} id Users unique ID.
- *
- * @apiSuccess {String} firstname Firstname of the User.
- * @apiSuccess {String} middlename  Lastname of the User.
- */
 
 
 /*This module is meant to house the functions
