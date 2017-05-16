30d909a17d99c24ccdabc1a4a23702516192a6d6
Byunggwan Lim <bg_lim@hotmail.com>
2017-05-15 19:16:58 -0700

    Fixing nightwatch problem

--- gulp/test/Nightwatch.js
+++ gulp/test/Nightwatch.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp'),
     nightwatch = require('gulp-nightwatch');
 
- gulp.task('test:nightwatch', () =>
-  gulp.src('test/*.js', {read: false})
+ gulp.task('test:nightwatch', function(){
+  return gulp.src('test/*.js', {read: false})
     .pipe(nightwatch({configFile: 'nightwatch.conf.BASIC.js'}))
     .once('error', () => {
-      process.exit(1);
+      process.exit(1)
     })
-);
+    .pipe(exit());
+});
