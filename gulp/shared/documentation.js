c0009c3678db83650e01db124c442b6f1d19b774
Andrew Chan <chan94306@gmail.com>
2017-05-15 17:34:51 -0700

    apidoc setup

---
+++ gulp/shared/documentation.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp'),
+    apidoc = require('gulp-apidoc'),
+    jsdoc = require('gulp-jsdoc3');
+
+// JS Documentation
+gulp.task('jsdoc', function (cb) {
+    gulp.src(['README.md', './client/assets/native/js/*.js'], {read: false})
+    .pipe(jsdoc(cb));
+});
+
+// API Documentation
+gulp.task('apidoc', function(done) {
+    apidoc({
+        src: "./server/routes/",
+        dest: "apidoc/"
+    }, done);
+});
