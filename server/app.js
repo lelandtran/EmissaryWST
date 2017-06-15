336adff74459be61aa931d8683e585e590a1a2a9
Andrew Chan <chan94306@gmail.com>
2017-06-15 13:39:18 -0700

    /apidoc live

--- server/app.js
+++ server/app.js
@@ -89,6 +89,9 @@ if(app.get('env') !== 'development') {
   app.use('/api/*', validate);
 }
 
+// documentation
+app.use('/apidoc', express.static(path.join(__dirname, '../apidoc')));
+
 app.get('/settings', function(req,res){
   res.sendFile(path.join(__dirname,'../dist/assets/views/settings.html'))
 });
@@ -154,7 +157,6 @@ server.listen(app.get('port'), function() {
 var server = socketIO.createServer(io);
 
 
-app.use('/apidoc', express.static(path.join(__dirname, 'apidoc')));
 
 
 module.exports = app;
